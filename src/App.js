39682b4f95e557483b555f7945eebddd882e8999
Pete Seeling <peteseeling@gmail.com>
2022-02-16 17:51:33 -0600

    Dev branch created. Auth page appearing

--- src/App.js
+++ src/App.js
@@ -13,25 +13,41 @@ import ListPage from './ListPage';
 import './App.css';
 
 export default function App() {
-  // track the user in state
 
+  // track the user in state
+  const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
   // add a useEffect to get the user and inject the user object into state on load
 
   async function handleLogout() {
     // call the logout function
+    logout();
     // clear the user in state
+    setUser('');
   }
 
   return (
     <Router>
       <div className='App'>
         <header>
-          {/* if there's a user, render a logout button here */}
+          {
+            user &&
+          <div>
+            <button onClick={handleLogout}>LogOut</button>
+          </div>
+          }
+           
+         
         </header>
         <main>
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {
+                user 
+                  ? <Redirect to="/ListPage" />
+                  : <AuthPage />
+
+              }
             </Route>
             <Route exact path="/shopping-list">
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
