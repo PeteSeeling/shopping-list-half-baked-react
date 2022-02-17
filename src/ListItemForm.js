853df1722c175f9d6cd4221f26e428aeaabba310
Pete Seeling <peteseeling@gmail.com>
2022-02-16 19:34:30 -0600

    working on listpage

--- src/ListItemForm.js
+++ src/ListItemForm.js
@@ -1,38 +1,51 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { createListItem } from './services/fetch-utils';
 
 export default function ListItemForm({ fetchItems }) {
   // you'll need to track the name and quantity in state
+  const [listItem, setListItem] = useState([]);
+  const [quantity, setQuantity] = useState('');
+  const [name, setName] = useState('');
+  const history = useHistory();
 
   async function handleSubmit(e) {
     e.preventDefault();
     
     // make a new list item in supabase using the form values stored in state
+    await createListItem({ name, quantity });
+    history.push('/items');
+   
 
+  
     // refetch the items using the handler functionpassed down as a prop
-
+    fetchItems();
+    setName('');
+    setQuantity(0);
+    
     // clear the name and quantity in state to refresh the form
   }
 
   return (
     <div className='new-item-form-container'>
       {/* on submit, call the handleSubmit function */}
-      <form>
+      <form onSubmit={handleSubmit}>
           I need . . . 
         <label>
             Quantity
           {/* on change, update the quantity in state */}
-          <input 
+          <input onChange={setQuantity}
             // this should be a controlled input, soi set the value based on state
             required 
             type="number" 
             name="quantity"
+            
           />
         </label>
         <label>
             Name
           {/* on change, update the name in state */}
-          <input
+          <input onChange={setName}
             // this should be a controlled input, soi set the value based on state 
             required 
             name="name" />
